15918717aeb7a6d219150ec83054e3175839bd3c
Darthex <76007809+Darthex@users.noreply.github.com>
2021-11-25 12:09:06 +0530

    long_response.py

--- long_responses.py
+++ long_responses.py
@@ -1,4 +1,6 @@
 import random
+import datetime as dt
+
 
 lr_Advice = 'I am not an expert at giving advices, maybe you should ask the internet'
 lr_eat = 'You should check the map!, there are some yummy restaurants nearby'
@@ -6,7 +8,12 @@ lr_age = 'I am a bot, i do not age, but my owners are fairly young'
 lr_who = 'I am your friendly neighbourhood CHATBOT!!, i am here to listen to all your ramblings'
 lr_joke = 'I was playing chess with my friend and he said, “Let’s make this interesting." ' \
           '         So we stopped playing chess.'
-lr_day = 'Its Thursday today'
+
+today = dt.date.today()
+day= dt.date.today().strftime('%A')
+
+lr_date = 'Date for today is ' + str(today)
+lr_day = 'Its ' + str(day) + ' today'
 lr_smart = 'I am not infused with Ai right now, but i am fairly confident about my intelligence'
 lr_lang = 'I am fluent in English and sarcasm'
 
@@ -17,4 +24,5 @@ def unknown():
                 "Sounds about right",
                 "What does that mean?"][random.randrange(4)]
     return response
+
 
