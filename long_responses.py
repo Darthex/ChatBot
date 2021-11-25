612354f3909cae6f7e9da424a0707148149490c1
Darthex <76007809+Darthex@users.noreply.github.com>
2021-11-25 10:32:36 +0530

    Update long_responses.py

--- long_responses.py
+++ long_responses.py
@@ -1,5 +1,15 @@
 import random
 
+lr_Advice = 'I am not an expert at giving advices, maybe you should ask the internet'
+lr_eat = 'You should check the map!, there are some yummy restaurants nearby'
+lr_age = 'I am a bot, i do not age, but my owners are fairly young'
+lr_who = 'I am your friendly neighbourhood CHATBOT!!, i am here to listen to all your ramblings'
+lr_joke = 'I was playing chess with my friend and he said, “Let’s make this interesting." ' \
+          '         So we stopped playing chess.'
+lr_day = 'Its Thursday today'
+lr_smart = 'I am not infused with Ai right now, but i am fairly confident about my intelligence'
+lr_lang = 'I am fluent in English and sarcasm'
+
 
 def unknown():
     response = ['Could you please re-phrase that?',
@@ -7,3 +17,4 @@ def unknown():
                 "Sounds about right",
                 "What does that mean?"][random.randrange(4)]
     return response
+
